bc63399e0ad166de53beb9db6818e13f230c07f0
asad-ullah-turab <asadullahturab47@gmail.com>
2026-04-21 17:46:13 +0500

    Fix: spelling mistake in signup route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
     <Routes>
       <Route index element={<Home />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/singup" element={<Signup />} />
+      <Route path="/signup" element={<Signup />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
